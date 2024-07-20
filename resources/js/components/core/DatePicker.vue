95242a5c5e294f0a60e694a144c8e9e8f4cbdf75
Sayed <sayed_sayedzada@yahoo.com>
2024-07-20 15:42:01 +0900

    New fields added to construction form

--- resources/js/components/core/DatePicker.vue
+++ resources/js/components/core/DatePicker.vue
@@ -2,8 +2,8 @@
     <div>
         <BaseLabel v-if="label" class="block pl-3 text-sm font-medium leading-6 text-gray-900">{{
             label }}</BaseLabel>
-        <VueDatePicker v-model="date" auto-apply :placeholder="placeholder" :format="format" :enable-time-picker="false" locale="ja" class="mx-2 my-2"
-             />
+        <VueDatePicker v-model="date" auto-apply :placeholder="placeholder" :format="format" :enable-time-picker="false"
+            locale="ja" class="mx-2 my-2" />
     </div>
 
 </template>
@@ -14,6 +14,7 @@ import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 
 const date = ref();
+
 const props = defineProps({
     label: {
         type: String,
@@ -28,4 +29,6 @@ const props = defineProps({
         required: true
     }
 })
+
+
 </script>
--- resources/js/views/projects/Create.vue
+++ resources/js/views/projects/Create.vue
@@ -6,14 +6,21 @@
       </template>
       <form @submit.prevent="saveProject">
         <BaseInput type="text" :label="$t('project.code')" v-model="model.code" :error="errors?.code" readonly />
-        <BaseInput type="text" :label="$t('project.temporary_name')" v-model="model.temporary_name" :placeholder="$t('project.construction_example')" :error="errors?.code"/>
-        <BaseInput type="text" :label="$t('project.confirmed_name')" v-model="model.confirmed_name" :placeholder="$t('project.construction_example')" :error="errors?.code"/>
-        <BaseInput type="text" :label="$t('project.short_name')" v-model="model.confirmed_name" :placeholder="$t('project.construction_example')" :error="errors?.code"/>
-        <DatePicker :placeholder="$t('general.select_date')" label="Select Date"/>
+        <BaseInput type="text" :label="$t('project.temporary_name')" v-model="model.temporary_name"
+          :placeholder="$t('project.construction_example')" :error="errors?.code" />
+        <BaseInput type="text" :label="$t('project.confirmed_name')" v-model="model.confirmed_name"
+          :placeholder="$t('project.construction_example')" :error="errors?.code" />
+        <BaseInput type="text" :label="$t('project.short_name')" v-model="model.confirmed_name"
+          :placeholder="$t('project.construction_example')" :error="errors?.code" />
+        <BaseSelect :options="construction_types" :label="$t('project.construction_type')" v-model="model.type" />
+        <BaseSelect :options="natures" :label="$t('project.construction_nature')" v-model="model.nature" />
+        <DatePicker :placeholder="now()" :label="$t('project.construction_start_date')" v-model="model.start_date" />
+        <DatePicker :placeholder="now(30)" :label="$t('project.construction_complete_date')"
+          v-model="model.complete_date" />
         <BaseButton>{{ $t("project.submit") }}</BaseButton>
       </form>
     </PageComponent>
-    
+
   </div>
 </template>
 <script setup>
@@ -26,6 +33,7 @@ import BaseButton from "@/components/core/BaseButton.vue";
 import { Core as YubinBangoCore } from "yubinbango-core2";
 import store from "@/store/index.js";
 import DatePicker from "@/components/core/DatePicker.vue";
+import BaseSelect from "@/components/core/BaseSelect.vue";
 
 const router = useRouter();
 const errors = ref({});
@@ -34,6 +42,10 @@ const model = reactive({
   temporary_name: "",
   confirmed_name: "",
   short_name: "",
+  type: "",
+  nature: "",
+  start_date: "",
+  complete_date: "",
   name_kana: "",
   fax: "",
   phone: "",
@@ -43,6 +55,17 @@ const model = reactive({
   area: "",
   street: "",
 });
+
+const construction_types = [
+  { value: "1", title: '公共工事' },
+  { value: "2", title: '民間工事' },
+  { value: "3", title: 'その他' }];
+
+const natures = [
+  { value: "10", title: "土木一式" },
+  { value: "20", title: "建築一式" },
+  { value: "290", title: "解体" }
+]
 
 model.code = computed(() => store.state.projects.availableCode);
 
@@ -76,5 +99,19 @@ function saveProject() {
         errors.value = error.response.data.errors;
       }
     });
+}
+
+function now(next = 0) {
+  const currentDate = new Date();
+
+  if (typeof next === 'number' && next !== 0) {
+    currentDate.setDate(currentDate.getDate() + next);
+  }
+
+  return currentDate.toLocaleDateString("ja-JP", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit"
+  }).replaceAll('/', '-');
 }
 </script>
